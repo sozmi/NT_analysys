44546020c7996de77c687d4275f8071d4a641eb3
Михаил Созонтов <sozontovmisha@mail.ru>
2024-09-15 12:41:46 +0300

    написание основного метода

--- NT_lab1/main.py
+++ NT_lab1/main.py
@@ -1,6 +1,16 @@
 
+from DataManager import DataManager
+
 def main():
-  print("test")
+    queries = ["polar bear","brown bear"]
+    needCount = 1000
+    for query in queries:
+        while True:
+            if(DataManager.clearData(query)>=needCount):
+                break
+            DataManager.downloadFound(query,needCount);
+        
+       
 
 if __name__ == '__main__':
     main()
