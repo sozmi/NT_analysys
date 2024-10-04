7e6895139fae8a435b1c62eabbf7c22fd22687ca
Михаил Созонтов <sozmi@mail.ru>
2024-10-04 10:34:39 +0300

    создание файла конфигурации

--- NT_analysis/main.py
+++ NT_analysis/main.py
@@ -1,11 +1,22 @@
+from email.mime import image
 from managers.DataManager import DataManager as dm
+from bs4 import BeautifulSoup
 
 def main():
-    queries = ["polar bear","brown bear"]
-    needCount = 1000
+    fd = open('config.xml', 'r')
+    xml_file = fd.read() 
+    soup = BeautifulSoup(xml_file, 'lxml')
+    config = soup.find('config')
+    need_count = int(config.find('image')['count'])
+    queries = []
+    q = config.find('queries')
+    for query in q.findAll('query'):
+        queries.append(query["text"])
+
+    print(f'Необходимо скачать {need_count} изображений по запросам: {queries}')
                    
     for query in queries:
-        dm.downloadImages(query, needCount);
+        dm.downloadImages(query, need_count);
         dm.reinitIndexs(query)
               
 
