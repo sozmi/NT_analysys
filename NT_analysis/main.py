efdb31eb3eb9fad5fe675edde60caa7eb8539137
Михаил Созонтов <sozmi@mail.ru>
2024-10-04 11:10:43 +0300

    добавление класса-конфигурации

--- NT_analysis/main.py
+++ NT_analysis/main.py
@@ -1,20 +1,12 @@
 from email.mime import image
-from managers.DataManager import DataManager as dm
-from bs4 import BeautifulSoup
+from managers.DataManager import DataManager  as dm
+from managers.ConfigManager import ConfigManager as cm
 
-def main():
-    fd = open('config.xml', 'r')
-    xml_file = fd.read() 
-    soup = BeautifulSoup(xml_file, 'lxml')
-    config = soup.find('config')
-    need_count = int(config.find('image')['count'])
-    queries = []
-    q = config.find('queries')
-    for query in q.findAll('query'):
-        queries.append(query["text"])
 
-    print(f'Необходимо скачать {need_count} изображений по запросам: {queries}')
-                   
+def main():     
+    conf = cm()
+    need_count = conf.need_count
+    queries = conf.queries
     for query in queries:
         dm.downloadImages(query, need_count);
         dm.reinitIndexs(query)
